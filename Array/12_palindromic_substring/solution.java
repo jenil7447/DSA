108c35287156e2cf61189ea708327b38d2b5c073
Jenil Jethva <151537234+jenil7447@users.noreply.github.com>
2024-11-06 18:42:28 +0530

    Create solution.java

---
+++ Array/12_palindromic_substring/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            
+            count += expandFromCenter(s, i, i);
+            count += expandFromCenter(s, i, i + 1);
+        }
+        return count;
+    }
+
+    private int expandFromCenter(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;  
+            left--;   
+            right++;  
+        }
+        return count;
+    }
+}
