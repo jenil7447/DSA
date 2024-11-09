707f8733ce60233356ef145d3affe304073b59c5
Jenil Jethva <151537234+jenil7447@users.noreply.github.com>
2024-11-09 20:24:48 +0530

    Create Solution.java

---
+++ Stack/07_Car_Fleet/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        float[] time = new float[target + 1];
+        
+        for(int i=0 ; i<speed.length ; i++){
+            time[position[i]] = (float)(target - position[i]) / speed[i];
+        }
+        
+        int count = 0;
+        float maxi = 0;
+        for (int i=time.length-1 ; i>=0 ; i--){
+            if(time[i] > maxi){
+                count++;
+                maxi = time[i];
+            }
+        }
+      
+        return count;
+    }
+}
+// Time complexity: O(n); Space Complexity: O(n);
