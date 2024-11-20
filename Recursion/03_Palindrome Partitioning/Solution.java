02c9d74307cac1b4882d865c738afde5b1fd9a6c
Jenil Jethva <151537234+jenil7447@users.noreply.github.com>
2024-11-20 21:25:44 +0530

    Create Solution.java

---
+++ Recursion/03_Palindrome Partitioning/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(String s, int start, List<String> current, List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                current.add(s.substring(start, end + 1));
+                backtrack(s, end + 1, current, result);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low++) != s.charAt(high--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
