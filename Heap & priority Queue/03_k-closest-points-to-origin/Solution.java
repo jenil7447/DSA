7c38d86a124f1d0afe3fc8125433512dda9050e1
Jenil Jethva <151537234+jenil7447@users.noreply.github.com>
2024-12-11 20:29:00 +0530

    Create Solution.java

---
+++ Heap & priority Queue/03_k-closest-points-to-origin/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {  
+            int distA = a[0] * a[0] + a[1] * a[1];
+            int distB = b[0] * b[0] + b[1] * b[1];
+            return distB - distA; 
+        });
+        for (int[] point : points) {
+            maxHeap.add(point); 
+            if (maxHeap.size() > k) {
+                maxHeap.poll(); 
+            }
+        }
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = maxHeap.poll();
+        }
+        return result;
+    }
+}
