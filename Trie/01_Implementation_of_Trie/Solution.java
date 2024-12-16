d792f87c7a5bbded264e3b4413a8b6ceb77c19ac
Jenil Jethva <151537234+jenil7447@users.noreply.github.com>
2024-12-16 19:56:08 +0530

    Create Solution.java

---
+++ Trie/01_Implementation_of_Trie/Solution.java
@@ -0,0 +1,79 @@
+class Node {
+    Node[] link = new Node[26];
+    boolean flag = false;
+
+    public Node() {
+    }
+
+    public boolean containsKey(char ch) {
+        return link[ch - 'a'] != null;
+    }
+
+    public Node get(char ch) {
+        return link[ch - 'a'];
+    }
+
+    public void put(char ch, Node node) {
+        link[ch - 'a'] = node;
+    }
+
+    public void setEnd() {
+        flag = true;
+    }
+
+    public boolean isEnd() {
+        return flag;
+    }
+}
+
+class Trie {
+    Node root;
+
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (!node.containsKey(ch)) {
+                node.put(ch, new Node());
+            }
+            node = node.get(ch);
+        }
+        node.setEnd(); 
+    }
+
+    public boolean search(String word) {
+        Node node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (!node.containsKey(ch)) {
+                return false;
+            }
+            node = node.get(ch);
+        }
+        return node.isEnd(); 
+    }
+
+    public boolean startsWith(String prefix) {
+        Node node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            if (!node.containsKey(ch)) {
+                return false;
+            }
+            node = node.get(ch);
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
