2fff2eb87f03bb6de357c2e00dfd9667e932bc21
Jenil Jethva <151537234+jenil7447@users.noreply.github.com>
2025-01-22 19:12:58 +0530

    Create Solution.java

---
+++ Greedy/01_Jump _Game/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int maxReach = 0;
+        for(int i = 0; i < n; i++){
+            if( i > maxReach) return false;
+            maxReach = Math.max(maxReach,i + nums[i]);
+            if(maxReach >= n-1) return true;
+        }
+        return true;
+    }
+}
