15d49fd37f002fef49c57f13ffcde5e4a279e577
Jenil Jethva <151537234+jenil7447@users.noreply.github.com>
2026-08-10 19:46:04 +0530

    Add function to detect cycle in linked list
    Implement cycle detection in a linked list using Floyd's Tortoise and Hare algorithm.

---
+++ Linked List/Linked_List_Cycle-2/solution.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let slow = head;
+    let fast = head;
+    while(fast !== null && fast.next !== null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast){
+            let ptr = head;
+            while(ptr !== slow){
+                ptr = ptr.next;
+                slow = slow.next;
+            } 
+            return slow;
+        }
+    }
+    return null;
+};
